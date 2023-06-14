cb3959981d15c8ea87be3199b09739646f9be866
Nobu C. Shirai <5637009+nobucshirai@users.noreply.github.com>
2023-06-14 11:33:04 +0900

    Updated.

--- pandoc_markdown.py
+++ pandoc_markdown.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-
+# -*- coding: utf-8 -*-
 import argparse
 import subprocess
 import sys
 import re
+import os
+from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser(description='Convert a markdown file using pandoc, then replace some strings with sed.')
     parser.add_argument('input_file', type=str, help='The markdown file to be converted.')
-    parser.add_argument('-o', '--output_file', type=str, default=None, help='Specify the output filename. If not specified, the result will be printed to stdout.')
+    parser.add_argument('-o', '--output_file', nargs='?', const=True, default=False, help='Specify the output filename. If not specified, the result will be printed to stdout.')
     args = parser.parse_args()
 
     pandoc_command = ['pandoc', '-s', args.input_file, '--mathjax', '--shift-heading-level-by=-1']
@@ -20,10 +22,21 @@ def main():
                     r'text-rendering: optimizeLegibility;\n      font-family: Arial, sans-serif;', 
                     pandoc_output.decode('utf-8'))
 
-    if args.output_file is None:
+    if args.output_file is False: # No -o argument provided, print to stdout
         sys.stdout.write(output)
-    else:
-        with open(args.output_file, 'w') as f:
+    else: 
+        if args.output_file is True: # -o argument provided, but no filename specified
+            output_file = Path(args.input_file).stem + '.html'
+        else: # -o argument provided, and a filename specified
+            output_file = args.output_file
+        
+        if os.path.exists(output_file):
+            overwrite = input(f"File {output_file} already exists. Do you want to overwrite it? (yes/no): ")
+            if overwrite.lower() != 'yes':
+                print('Aborted.')
+                return
+
+        with open(output_file, 'w') as f:
             f.write(output)
 
 if __name__ == "__main__":
